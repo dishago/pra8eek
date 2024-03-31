3656a9cffa05429c6b6691953c7403e6472e25f1
Disha Goyal <deck@192.168.1.10>
2024-03-31 12:41:43 +0530

    basic template

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('a').on('click', function(event) {
+        if (this.hash !== '') {
+            event.preventDefault();
+            const hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800);
+        }
+    });
+});
